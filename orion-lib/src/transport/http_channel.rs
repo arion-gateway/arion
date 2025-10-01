f52165efb2d4986dd54b9d8513421d3ae06947d9
Nicola Bonelli <nicola.bonelli@huawei-partners.com>
2025-10-01 12:11:31 +0200

    Set HTTP/2 initial max send streams in client builder

--- orion-lib/src/transport/http_channel.rs
+++ orion-lib/src/transport/http_channel.rs
@@ -292,6 +292,7 @@ impl HttpChannelBuilder {
             client_builder.http2_initial_stream_window_size(http2_options.initial_stream_window_size());
 
             if let Some(max) = http2_options.max_concurrent_streams() {
+                client_builder.http2_initial_max_send_streams(max);
                 client_builder.http2_max_concurrent_streams(max);
             }
         }
