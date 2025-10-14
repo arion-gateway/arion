adf67967e71cb25a6872172166111781f925a4c3
Nicola Bonelli <nicola.bonelli@huawei-partners.com>
2025-10-14 09:53:46 +0200

    Cargo fmt.

--- orion-lib/src/body/body_with_timeout.rs
+++ orion-lib/src/body/body_with_timeout.rs
@@ -55,7 +55,10 @@ where
     B: std::fmt::Debug,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("BodyWithTimeout").field("timeout", &self.timeout).field("body", &self.body).finish_non_exhaustive()
+        f.debug_struct("BodyWithTimeout")
+            .field("timeout", &self.timeout)
+            .field("body", &self.body)
+            .finish_non_exhaustive()
     }
 }
 
