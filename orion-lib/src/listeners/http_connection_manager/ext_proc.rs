4dfd73d7cddc4c805a49859951a7ec672a1ffbea
Nicola Bonelli <nicola.bonelli@huawei-partners.com>
2025-11-18 15:40:01 +0100

    Cargo fmt.

--- orion-lib/src/listeners/http_connection_manager/ext_proc.rs
+++ orion-lib/src/listeners/http_connection_manager/ext_proc.rs
@@ -327,7 +327,6 @@ impl ExternalProcessor {
             },
         };
 
-
         debug!(target: "ext_proc", "apply_request completed: {res:?}!");
         request.body_mut().inner.wait_frame().await;
         res
