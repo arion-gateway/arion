21bf4968ddc7f24f3b4f13ec06e8695480583a0d
Nicola Bonelli <nicola.bonelli@huawei-partners.com>
2026-02-10 12:43:55 +0100

    Remove once_cell dependency in favor of std::sync::OnceLock

--- orion-lib/src/access_log.rs
+++ orion-lib/src/access_log.rs
@@ -23,7 +23,7 @@ pub mod logger;
 mod pool;
 
 use logger::AccessLogger;
-use once_cell::sync::OnceCell;
+use std::sync::OnceLock;
 use orion_configuration::config::network_filters::access_log::AccessLogConf;
 use orion_format::FormattedMessage;
 use parking_lot::Mutex;
@@ -75,7 +75,7 @@ pub enum LoggerError {
     SenderError,
 }
 
-static SENDER_POOL: OnceCell<LoggerPool<AccessLogMessage>> = OnceCell::new();
+static SENDER_POOL: OnceLock<LoggerPool<AccessLogMessage>> = OnceLock::new();
 
 pub type AccessLogPermit = Permit<'static, AccessLogMessage>;
 pub type ShareableAccessLogPermit = Arc<Mutex<Option<AccessLogPermit>>>;
