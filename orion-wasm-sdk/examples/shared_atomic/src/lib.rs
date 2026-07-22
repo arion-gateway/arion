3c1902e3904fa8fe2e7275fc3ce20d2c7265a036
Nicola Bonelli <nicola.bonelli@huawei-partners.com>
2026-07-22 15:51:23 +0200

    Add shared_atomic example using AtomicU64

---
+++ orion-wasm-sdk/examples/shared_atomic/src/lib.rs
@@ -0,0 +1,55 @@
+use orion_wasm_sdk::{init_tracing, orion_plugin, FilterAction, HeaderMutation, HttpHeaders, Plugin, RequestHandle};
+use orion_wasm_sdk::shared::AtomicU64;
+use tracing::{debug, error, info};
+use std::sync::atomic::Ordering;
+
+#[derive(Default)]
+struct SharedAtomicFilter {
+    counter: Option<AtomicU64>,
+}
+
+#[orion_plugin]
+impl Plugin for SharedAtomicFilter {
+    fn on_plugin_start(&mut self) {
+        let _ = init_tracing();
+        debug!("SharedAtomicFilter: on_plugin_start");
+
+        match AtomicU64::try_new("request_counter") {
+            Ok(atomic) => {
+                info!("Successfully created/opened shared atomic variable 'request_counter'");
+                self.counter = Some(atomic);
+            }
+            Err(e) => {
+                error!("Failed to open shared atomic variable: {:?}", e);
+            }
+        }
+    }
+
+    fn on_request_headers(&mut self, ctx: &RequestHandle<HttpHeaders>) -> FilterAction {
+        if let Some(counter) = &self.counter {
+            // Increment the shared counter by 1
+            let prev = counter.fetch_add(1, Ordering::SeqCst);
+            let current = prev + 1;
+            info!("Shared counter incremented! Previous value: {}, New value: {}", prev, current);
+
+            // Set the result as an HTTP header sent to the upstream
+            let header_value_str = format!("{}", current);
+            if let Ok(header_value) = http::header::HeaderValue::from_str(&header_value_str) {
+                if let Err(e) = ctx.apply_header_mutations(&[
+                    HeaderMutation::Set(
+                        http::header::HeaderName::from_static("x-request-counter"),
+                        header_value,
+                    )
+                ]) {
+                    error!("Failed to set x-request-counter header: {:?}", e);
+                }
+            } else {
+                error!("Failed to create header value from counter");
+            }
+        } else {
+            error!("Shared counter is not initialized!");
+        }
+
+        FilterAction::Continue
+    }
+}
