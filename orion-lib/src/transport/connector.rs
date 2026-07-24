81b621813bc04ebccb342bf684838d6ae948d47a
Francesco Ciaccia <francesco.ciaccia1@huawei-partners.com>
2026-07-24 12:18:53 +0200

    Fix IPv6 socket creation using new_v6

--- orion-lib/src/transport/connector.rs
+++ orion-lib/src/transport/connector.rs
@@ -200,7 +200,7 @@ impl LocalConnectorWithDNSResolver {
                         })
                         .map_into()
                 })?,
-                std::net::SocketAddr::V6(_) => TcpSocket::new_v4().map_err(|e| {
+                std::net::SocketAddr::V6(_) => TcpSocket::new_v6().map_err(|e| {
                     ContextualError::new(e)
                         .with_context_data(TcpErrorContext {
                             upstream_addr: addr,
